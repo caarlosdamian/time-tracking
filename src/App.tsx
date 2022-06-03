2cfdc22c417e4d667e1b99a23cb1ca9aabe539bd
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-06-02 22:58:54 -0500

    finished application

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,70 @@
 import Avatar from "./images/image-jeremy.png";
-import Ellipsis from "./images/icon-ellipsis.svg"
+import Ellipsis from "./images/icon-ellipsis.svg";
 import { data } from "./data";
 import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState<"daily" | "weekly" | "monthly">("weekly");
+
+  const arrays = ["daily", "weekly", "monthly"];
+
   return (
     <div className="main__container">
       <div className="grid__wrapper">
         <div className="grid__left">
           <div className="grid__left-container__absolute"></div>
-          <div className="grid__left-img__container">
-            <img src={Avatar} alt="Avatar" className="grid__left-img" />
+          <div className="top--container">
+            <div className="grid__left-img__container">
+              <img src={Avatar} alt="Avatar" className="grid__left-img" />
+            </div>
+            <div className="top--container-header">
+              <h2 className="grid__left-header">Report for</h2>
+              <h1 className="grid__left-subtitle">Jeremy Robson</h1>
+            </div>
           </div>
-          <h2 className="grid__left-header">Report for</h2>
-          <h1 className="grid__left-subtitle">Jeremy Robson</h1>
+
           <div className="grid__left-button__container">
-            <button className="grid__left-button active">Daily</button>
-            <button className="grid__left-button">Weekly</button>
-            <button className="grid__left-button">Monthly</button>
+            <button
+              className={`grid__left-button ${time === "daily" && "active"}`}
+              onClick={() => setTime("daily")}
+            >
+              Daily
+            </button>
+            <button
+              className={`grid__left-button ${time === "weekly" && "active"}`}
+              onClick={() => setTime("weekly")}
+            >
+              Weekly
+            </button>
+            <button
+              className={`grid__left-button ${time === "monthly" && "active"}`}
+              onClick={() => setTime("monthly")}
+            >
+              Monthly
+            </button>
           </div>
         </div>
         {data.map((item) => (
           <div className="grid__item">
             <div className="grid__item-container-bottom">
               <div className="grid__item-top">
-                <h2 className="grid__item-top__header">Exercise</h2>
-                <img src={Ellipsis} alt="Ellipsis" className="grid__item-top__img" />
+                <h2 className="grid__item-top__header">{item.title}</h2>
+                <img
+                  src={Ellipsis}
+                  alt="Ellipsis"
+                  className="grid__item-top__img"
+                />
               </div>
-              <h1 className="grid__item-header">
-                {item.timeframes.daily.current}hrs
-              </h1>
-              <span className="grid__item-bottom">
-                Last Week - {item.timeframes.daily.previous}hrs
-              </span>
+              <div className="grid__midle-container">
+                <h1 className="grid__item-header">
+                  {item.timeframes[`${time}`].current}hrs
+                </h1>
+                <span className="grid__item-bottom">
+                  Last Week - {item.timeframes[`${time}`].previous}
+                  hrs
+                </span>
+              </div>
             </div>
             <div className={`grid__item-container ${item.color}`}>
               <img src={item.icon} alt="" className="grid__item-img" />
