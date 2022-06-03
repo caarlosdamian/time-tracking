f125560a105803c59e3afb472f8721761612aab1
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-06-02 22:04:46 -0500

    finished styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-
 import Avatar from "./images/image-jeremy.png";
-import {data} from "./data";
+import Ellipsis from "./images/icon-ellipsis.svg"
+import { data } from "./data";
 import "./App.scss";
 
 function App() {
@@ -23,9 +23,19 @@ function App() {
         {data.map((item) => (
           <div className="grid__item">
             <div className="grid__item-container-bottom">
+              <div className="grid__item-top">
+                <h2 className="grid__item-top__header">Exercise</h2>
+                <img src={Ellipsis} alt="Ellipsis" className="grid__item-top__img" />
+              </div>
+              <h1 className="grid__item-header">
+                {item.timeframes.daily.current}hrs
+              </h1>
+              <span className="grid__item-bottom">
+                Last Week - {item.timeframes.daily.previous}hrs
+              </span>
             </div>
             <div className={`grid__item-container ${item.color}`}>
-            <img src={item.icon} alt="" className="grid__item-img" />
+              <img src={item.icon} alt="" className="grid__item-img" />
             </div>
           </div>
         ))}
